4b09eef5022d8e19b6462dc5f9f7a0be36046a63
Guilherme F Maurila <gfmaurila@gmail.com>
2024-01-18 20:35:15 -0300

    -

--- Program.cs
+++ Program.cs
@@ -1,8 +1,30 @@
+using MongoDB.Driver;
+using poc.api.mongodb.Configuration;
+using poc.api.mongodb.EndPoints;
+using poc.api.mongodb.Service;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Swagger
+builder.Services.AddConnections();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerConfig(builder.Configuration);
+
+// Configure MongoDB
+var settings = builder.Configuration.GetSection("MongoDB").Get<MongoDbSettings>();
+builder.Services.AddSingleton(settings);
+builder.Services.AddSingleton<IMongoClient, MongoClient>(sp => new MongoClient(settings.ConnectionString));
+builder.Services.AddScoped(sp => new MongoDatabaseFactory(sp.GetRequiredService<IMongoClient>(), settings.Database));
+builder.Services.AddScoped(sp => sp.GetRequiredService<MongoDatabaseFactory>().GetDatabase());
+
+// Service
+builder.Services.AddScoped<IProdutoService, ProdutoService>();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.RegisterProdutosEndpoints();
 
 app.UseAuthorization();
 
